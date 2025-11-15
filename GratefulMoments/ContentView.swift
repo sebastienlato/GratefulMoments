21a2835f51cb57bc1c59bd5ab2a3f70a7ae402f6
Sebastien <sebastienlato@gmail.com>
2025-11-15 18:33:35 -0500

    Collect, model, and store data

--- GratefulMoments/ContentView.swift
+++ GratefulMoments/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Create a Grateful Moment") {
+                isPresented = true
+            }
+            .buttonStyle(.bordered)
+            .sheet(isPresented: $isPresented) {
+                MomentEntryView()
+            }
         }
         .padding()
     }
--- GratefulMoments/GratefulMomentsApp.swift
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import SwiftData
+
 
 @main
 struct GratefulMomentsApp: App {
+    let dataContainer = DataContainer()
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(dataContainer)
         }
+        .modelContainer(dataContainer.modelContainer)
     }
 }
