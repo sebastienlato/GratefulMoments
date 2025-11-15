d7e7d58aecfaf38cdb0076a28b6fdcdcf704d351
Sebastien <sebastienlato@gmail.com>
2025-11-15 18:47:57 -0500

    Build a detail view

--- GratefulMoments/Tabs/Moments/MomentsView.swift
+++ GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -49,7 +49,11 @@ struct MomentsView: View {
 
     private var pathItems: some View {
         ForEach(moments) { moment in
-            Text(moment.title)
+            NavigationLink {
+                MomentDetailView(moment: moment)
+            } label: {
+                Text(moment.title)
+            }
         }
     }
 }
