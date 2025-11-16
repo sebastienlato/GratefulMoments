c86bbaf67ea0dd5eae85ad8cc96441c4326e5aa6
Sebastien <sebastienlato@gmail.com>
2025-11-15 22:54:10 -0500

    Examine a bug with the Xcode debugger

--- GratefulMoments/Logic/StreakCalculator.swift
+++ GratefulMoments/Logic/StreakCalculator.swift
@@ -27,12 +27,21 @@ struct StreakCalculator {
             .map(\.timestamp)
             .map { calendar.dateComponents([.day], from: $0, to: endOfToday) }
             .compactMap { $0.day }
+        print(daysAgoArray)
         
         
         var streak = 0
         for daysAgo in daysAgoArray {
             if daysAgo == streak {
+                print("Streak already here. Don't increase the streak.")
+                continue
+            } else if daysAgo == streak + 1 {
+                print("A moment exists the day after the current streak")
                 streak += 1
+                print("Increased streak to \(streak)")
+            } else {
+                print("Streak of \(streak) broken with daysAgo \(daysAgo)")
+                break
             }
         }
         
