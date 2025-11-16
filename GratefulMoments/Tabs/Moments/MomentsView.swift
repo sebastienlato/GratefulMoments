c62333c8af571063b995b1360f52d4649ba58a62
Sebastien <sebastienlato@gmail.com>
2025-11-15 19:04:25 -0500

    Arrange components in a custom layout

--- GratefulMoments/Tabs/Moments/MomentsView.swift
+++ GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -15,6 +15,9 @@ struct MomentsView: View {
     private var moments: [Moment]
     
     
+    static let offsetAmount: CGFloat = 70.0
+    
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -42,17 +45,30 @@ struct MomentsView: View {
                     }
                 }
             }
+            .defaultScrollAnchor(.bottom, for: .initialOffset)
+            .defaultScrollAnchor(.bottom, for: .sizeChanges)
+            .defaultScrollAnchor(.top, for: .alignment)
             .navigationTitle("Grateful Moments")
         }
     }
     
     
     private var pathItems: some View {
-        ForEach(moments) { moment in
+        ForEach(moments.enumerated(), id: \.0) { index, moment in
             NavigationLink {
                 MomentDetailView(moment: moment)
             } label: {
-                Text(moment.title)
+                if moment == moments.last {
+                    MomentHexagonView(moment: moment, layout: .large)
+                } else {
+                    MomentHexagonView(moment: moment)
+                        .offset(x: sin(Double(index) * .pi / 2) * Self.offsetAmount)
+                }
+            }
+            .scrollTransition { content, phase in
+                content
+                    .opacity(phase.isIdentity ? 1 : 0)
+                    .scaleEffect(phase.isIdentity ? 1 : 0.8)
             }
         }
     }
@@ -68,4 +84,5 @@ struct MomentsView: View {
 #Preview("No moments") {
     MomentsView()
         .modelContainer(for: [Moment.self])
+        .environment(DataContainer())
 }
