8ecbe93cb6c7575b35e20261a097c3c79b31624a
Sebastien <sebastienlato@gmail.com>
2025-11-15 19:17:32 -0500

    Build a view to display information about a badge.

---
+++ GratefulMoments/Tabs/Achievements/BadgeDetailView.swift
@@ -0,0 +1,39 @@
+//
+//  BadgeDetailView.swift
+//  GratefulMoments
+//
+//  Created by sebastien lato on 2025-11-15.
+//
+
+import SwiftUI
+
+
+struct BadgeDetailView: View {
+    var badge: Badge
+    
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(badge.details.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 140, height: 140)
+            Text(badge.details.title)
+                .font(.title.bold())
+            Text(badge.details.congratulatoryMessage)
+                .font(.body)
+            Spacer()
+        }
+        .padding()
+        .frame(width: 320, height: 410)
+        .multilineTextAlignment(.center)
+        .foregroundStyle(.white)
+        .background(badge.details.color.opacity(0.8))
+        .clipShape(RoundedRectangle(cornerRadius: 16.0))
+    }
+}
+
+
+#Preview {
+    BadgeDetailView(badge: .sample)
+}
